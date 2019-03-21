d09d5a7349c2130eb9b6a17b2ecdd29ed7eacd54
Saijitsu <zeromus16@gmail.com>
2019-03-21 16:15:24 +0100

    add image gallery

--- app/gallery.js
+++ app/gallery.js
@@ -1,6 +1,12 @@
-import { Viewer } from "./component/viewer.js";
-import { Thumbnails } from "./component/thumbnails.js";
-import { Component} from "./component/component.js"
+import {
+    Viewer
+} from "./component/viewer.js";
+import {
+    Thumbnails
+} from "./component/thumbnails.js";
+import {
+    Component
+} from "./component/component.js"
 
 // ici on a une syntaxe de class, mais en réalité il s'agit d'un prototype
 // rappel: javascript est centré sur le mécanisme de prototypage
@@ -13,7 +19,9 @@ export class Gallery extends Component {
         <div class="component viewer-component"></div>
         <div class="component thumbnails-component"></div>
         `;
-        const viewer = new Viewer(this.root.querySelector('.viewer-component'));
+        const viewerElt = this.root.querySelector('.viewer-component');
+        const viewer = new Viewer(viewerElt);
+        // ou const viewer = new Viewer(this.root.querySelector('.viewer-component'));
         viewer.init();
         const thumbnails = new Thumbnails(this.root.querySelector('.thumbnails-component'));
         thumbnails.init();
